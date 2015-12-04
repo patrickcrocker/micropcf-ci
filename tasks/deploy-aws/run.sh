31f9f0e7a74095f4446605b3222745c7e85d6f03
Emily Casey <ecasey@pivotal.io>
2015-12-04 15:49:20 -0500

    Provide cats-base-aws task with Vagrantfile
    [#108859988]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/deploy-aws/run.sh
+++ tasks/deploy-aws/run.sh
@@ -5,6 +5,7 @@ set -ex
 export AWS_SSH_PRIVATE_KEY_PATH=$PWD/key.pem
 echo "$AWS_SSH_PRIVATE_KEY" > "$AWS_SSH_PRIVATE_KEY_PATH"
 
+cp vagrantfile-ci/Vagrantfile-* Vagrantfile
 AWS_INSTANCE_NAME=concourse-cats vagrant up --provider=aws
 export $(vagrant ssh -c "grep '^DOMAIN=' /var/micropcf/setup" 2>/dev/null | tr -d '\r')
 
