f06da034f0f87a3f6006a769804d63d14b5d42e1
Emily Casey <ecasey@pivotal.io>
2015-12-04 17:01:38 -0500

    Quote keys in CATs config.json
    [#108859988]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/cats/run.sh
+++ tasks/cats/run.sh
@@ -5,16 +5,16 @@ set -ex
 domain=$(cat deploy/domain)
 cat <<EOF >config.json
 {
-  api: "api.$domain",
-  admin_user: "admin",
-  admin_password: "admin",
-  apps_domain: "$domain",
-  system_domain: "$domain",
-  client_secret: "gorouter-secret",
-  skip_ssl_validation: true,
-  default_timeout: 600,
-  long_curl_timeout: 600,
-  use_http: true
+  "api": "api.$domain",
+  "admin_user": "admin",
+  "admin_password": "admin",
+  "apps_domain": "$domain",
+  "system_domain": "$domain",
+  "client_secret": "gorouter-secret",
+  "skip_ssl_validation": true,
+  "default_timeout": 600,
+  "long_curl_timeout": 600,
+  "use_http": true
 }
 EOF
 export CONFIG=$PWD/config.json
