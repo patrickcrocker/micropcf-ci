5862a34b68b5cb034081e73888592741d06d4f0f
Keaty Gross <kgross@pivotal.io>
2015-12-08 15:04:16 -0500

    wip - sleep maybe?
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- tasks/build-images/run.sh
+++ tasks/build-images/run.sh
@@ -19,6 +19,8 @@ echo "current_box_commit: " + $current_box_commit
 next_box_commit=$(git -C "$DISTRO-image-changes" rev-parse -q --verify HEAD)
 echo "next_box_commit: " + $next_box_commit
 
+sleep 60
+
 if [[ $current_box_commit == $next_box_commit ]]; then
   echo "$current_box_commit == $next_box_commit"
   echo -n $current_box_commit > box-commit
