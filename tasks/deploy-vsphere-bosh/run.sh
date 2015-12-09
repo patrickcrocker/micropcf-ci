5ef5f4de80a8c2b151c63da76390630b647b00a9
Stephen Levine <slevine@pivotal.io>
2015-12-09 12:08:10 -0500

    Preserve bosh-state.json for deploy-vsphere-bosh job
    [#109663774]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- tasks/deploy-vsphere-bosh/run.sh
+++ tasks/deploy-vsphere-bosh/run.sh
@@ -8,4 +8,6 @@ sed -i "s/VCENTER-ADDRESS/$VCENTER_ADDRESS/g" manifest.yml
 sed -i "s/VCENTER-USERNAME/$VCENTER_USERNAME/g" manifest.yml
 sed -i "s/VCENTER-PASSWORD/$VCENTER_PASSWORD/g" manifest.yml
 
+cp vsphere-bosh-state/bosh-state.json .
+
 bosh-init deploy manifest.yml
