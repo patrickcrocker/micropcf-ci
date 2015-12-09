1e69c13def8b51ed3d68b80d807884e2cb0af888
Mark DeLillo <mdelillo@pivotal.io>
2015-12-09 17:46:44 -0500

    Add job to build dev vmware boxes and upload to s3
    [#109733388]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

---
+++ tasks/build-dev-image/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+echo "$REMOTE_EXECUTOR_PRIVATE_KEY" > remote_executor.pem
+chmod 0600 remote_executor.pem
+
+set -x
+
+ssh-keyscan $REMOTE_EXECUTOR_ADDRESS >> $HOME/.ssh/known_hosts
+remote_path=$(ssh -i remote_executor.pem vcap@$REMOTE_EXECUTOR_ADDRESS mktemp -d /tmp/build-dev-image.XXXXXXXX)
+
+function cleanup { ssh -i remote_executor.pem vcap@$REMOTE_EXECUTOR_ADDRESS rm -rf "$remote_path"; }
+trap cleanup EXIT
+
+rsync -a -e "ssh -i remote_executor.pem" image-changes vcap@$REMOTE_EXECUTOR_ADDRESS:$remote_path/
+rm -rf image-changes || true
+
+ssh -i remote_executor.pem vcap@$REMOTE_EXECUTOR_ADDRESS <<EOF
+  cd "$remote_path"
+  "image-changes/images/$DISTRO/build" -only=vmware-iso -var 'dev=true'
+EOF
+
+rsync -e "ssh -i remote_executor.pem" vcap@$REMOTE_EXECUTOR_ADDRESS:$remote_path/image-changes/images/$DISTRO/output/micropcf-$DISTRO-vmware-v0.box $DISTRO.box
