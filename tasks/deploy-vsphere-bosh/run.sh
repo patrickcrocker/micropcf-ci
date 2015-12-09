118c43716163752cbe87a45dafe9fcaab6e7e83c
Mark DeLillo <mdelillo@pivotal.io>
2015-12-09 12:28:20 -0500

    Rename manifest.yml to bosh.yml for vsphere-deployment
    [#109663774]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/deploy-vsphere-bosh/run.sh
+++ tasks/deploy-vsphere-bosh/run.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-cp micropcf-ci/tasks/deploy-vsphere-bosh/manifest.yml .
+cp micropcf-ci/tasks/deploy-vsphere-bosh/bosh.yml .
 
-sed -i "s/VCENTER-ADDRESS/$VCENTER_ADDRESS/g" manifest.yml
-sed -i "s/VCENTER-USERNAME/$VCENTER_USERNAME/g" manifest.yml
-sed -i "s/VCENTER-PASSWORD/$VCENTER_PASSWORD/g" manifest.yml
+sed -i "s/VCENTER-ADDRESS/$VCENTER_ADDRESS/g" bosh.yml
+sed -i "s/VCENTER-USERNAME/$VCENTER_USERNAME/g" bosh.yml
+sed -i "s/VCENTER-PASSWORD/$VCENTER_PASSWORD/g" bosh.yml
 
 cp vsphere-bosh-state/bosh-state.json .
 
-bosh-init deploy manifest.yml
+bosh-init deploy bosh.yml
