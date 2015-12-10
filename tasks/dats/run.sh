5c4e6c58c5044eaec12da2506c7eb0f947f6ea0e
Stephen Levine <slevine@pivotal.io>
2015-12-10 10:18:00 -0500

    Create /etc/hosts entry for EC2 instance to eliminate xip.io flakes (for DATs)
    [#109665706]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- tasks/dats/run.sh
+++ tasks/dats/run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 export AWS_SSH_PRIVATE_KEY_PATH=$PWD/key.pem
 echo "$AWS_SSH_PRIVATE_KEY" > "$AWS_SSH_PRIVATE_KEY_PATH"
+
+set -x
+
+domain=$(cat deploy/domain)
+ip=${domain%.xip.io}
+
+echo $ip $domain > /etc/hosts
 
 git -C micropcf submodule update --init images/releases/diego-release
 git -C micropcf/images/releases/diego-release submodule update --init --recursive
 
 private_ip=$(cd deploy && vagrant ssh -c "ip route get 1 | awk '{print \$NF;exit}'" 2>/dev/null | tr -d '\r')
 
-./micropcf/bin/dats "$(cat deploy/domain)" "$private_ip"
+./micropcf/bin/dats $domain $private_ip
