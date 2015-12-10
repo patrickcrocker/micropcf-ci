91b7f5c664f923ee0c81f31db2a0f8328400ebce
Keaty Gross <kgross@pivotal.io>
2015-12-10 10:12:01 -0500

    Create /etc/hosts entry for EC2 instance to eliminate xip.io flakes
    [#109528076]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/cats/run.sh
+++ tasks/cats/run.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
+domain=$(cat deploy/domain)
+ip=${domain%.xip.io}
+
+echo $ip $domain > /etc/hosts
+
 git -C micropcf submodule update --init images/releases/cf-release
 git -C micropcf/images/releases/cf-release submodule update --init src/github.com/cloudfoundry/cf-acceptance-tests
 
-./micropcf/bin/cats $(cat deploy/domain)
+./micropcf/bin/cats $domain
