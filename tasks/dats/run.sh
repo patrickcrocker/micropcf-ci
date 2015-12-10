00ffcc90b17df8f1f72893eb5296157f60d221a1
Stephen Levine <slevine@pivotal.io>
2015-12-09 23:11:17 -0500

    Pull all diego-release submodules in DATs task
    [#109665706]

--- tasks/dats/run.sh
+++ tasks/dats/run.sh
@@ -6,9 +6,7 @@ export AWS_SSH_PRIVATE_KEY_PATH=$PWD/key.pem
 echo "$AWS_SSH_PRIVATE_KEY" > "$AWS_SSH_PRIVATE_KEY_PATH"
 
 git -C micropcf submodule update --init images/releases/diego-release
-git -C micropcf/images/releases/diego-release submodule update --init \
-  src/github.com/cloudfoundry-incubator/diego-acceptance-tests \
-  src/github.com/onsi/ginkgo
+git -C micropcf/images/releases/diego-release submodule update --init --recursive
 
 private_ip=$(cd deploy && vagrant ssh -c "ip route get 1 | awk '{print \$NF;exit}'" 2>/dev/null | tr -d '\r')
 
